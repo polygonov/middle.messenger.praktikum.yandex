10c055f4877d06087e6cff086eb7ce4599d78063
Igor Polyakov <IPolyakov@geonaft.ai>
2022-11-27 19:00:19 +0400

    update my regular tu russian letters

--- src/utils/validateRules.ts
+++ src/utils/validateRules.ts
@@ -2,7 +2,7 @@ export enum ValidateRules {
     login = '^(?=.*[A-Za-z])[-_A-Za-z0-9]{3,20}$',
     password = '^(?=.*[A-Z])(?=.*[0-9])[-_A-Za-z0-9]{8,40}$',
     email = '^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$',
-    name = '^[A-Z][A-Za-z]+$',
+    name = '^[A-ZА-Я][A-Za-zА-Яа-я]+$',
     phone = '^[+?0-9]{10,15}$',
     notEmpty = '^(?!s*$).+',
 }
