1928259b91a8a914658ccd708c019c77f8ba7876
Igor Polyakov <IPolyakov@geonaft.ai>
2022-11-28 02:48:05 +0400

    add css color variables

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import LoginPage from './pages/login';
 import { httpFetch } from './utils/HTTPTransport';
 import { renderDOM } from './utils/renderDOM';
+import './index.css';
 
 document.addEventListener('DOMContentLoaded', () => {
     const loginPage = new LoginPage();
