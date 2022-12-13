72a7a8fbc9775a43c876f6c6c50dce63b987791e
Igor Polyakov <IPolyakov@geonaft.ai>
2022-12-13 22:57:51 +0400

    remove unused pages, now SPA

--- src/change-password.ts
+++
@@ -1,7 +0,0 @@
-import { ChangePasswordPage } from './pages/change-password';
-import { renderDOM } from './utils/renderDOM';
-
-document.addEventListener('DOMContentLoaded', () => {
-    const сhangePasswordPage = new ChangePasswordPage();
-    renderDOM('#app', сhangePasswordPage);
-});
